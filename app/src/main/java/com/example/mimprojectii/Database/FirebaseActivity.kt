fd86cae5998ae53038fa27de57c97b6911cdc0a4
Matikones <matiadamek97@gmail.com>
2020-05-14 21:55:36 +0200

    Logout

--- app/src/main/java/com/example/mimprojectii/Database/FirebaseActivity.kt
+++ app/src/main/java/com/example/mimprojectii/Database/FirebaseActivity.kt
@@ -1,19 +1,65 @@
 package com.example.mimprojectii.Database
 
+import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.text.Layout
 import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.mimprojectii.MainActivity
 import com.example.mimprojectii.R
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 
-class FirebaseActivity : AppCompatActivity() {
+class FirebaseActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var auth: FirebaseAuth
+    lateinit var toolbar: Toolbar
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_firebase)
 
+        auth = FirebaseAuth.getInstance()
+
+        toolbar = findViewById(R.id.toolbarFb)
+        setSupportActionBar(toolbar)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navView = findViewById(R.id.nav_view)
+
+        val toggle = ActionBarDrawerToggle(
+        this, drawerLayout, toolbar, 0, 0
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        navView.setNavigationItemSelectedListener(this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        return super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.firebase, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId){
+            R.id.action_logout -> {
+                auth.signOut()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
