b56276a51f2cb7e33914b064b05660dd27460fb6
Matikones <matiadamek97@gmail.com>
2020-05-16 01:21:01 +0200

    Add menu and fragments

---
+++ app/src/main/java/com/example/mimprojectii/Books/BooksFragment.kt
@@ -0,0 +1,19 @@
+package com.example.mimprojectii.Books
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.mimprojectii.R
+
+class BooksFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_books, container, false)
+    }
+}
--- app/src/main/java/com/example/mimprojectii/Database/FirebaseActivity.kt
+++ app/src/main/java/com/example/mimprojectii/Database/FirebaseActivity.kt
@@ -1,19 +1,27 @@
 package com.example.mimprojectii.Database
 
 import android.content.Intent
+import android.graphics.Color
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.text.Layout
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import com.example.mimprojectii.Books.BooksFragment
+import com.example.mimprojectii.Games.GamesFragment
 import com.example.mimprojectii.MainActivity
+import com.example.mimprojectii.Movies.MoviesFragment
 import com.example.mimprojectii.R
+import com.example.mimprojectii.Series.SeriesFragment
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.app_bar_firebase.*
 
 class FirebaseActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -30,6 +38,9 @@ class FirebaseActivity : AppCompatActivity(), NavigationView.OnNavigationItemSel
 
         toolbar = findViewById(R.id.toolbarFb)
         setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        toolbar.title = "Projekt"
+        //toolbar.setTitleTextColor(Color.YELLOW)
 
         drawerLayout = findViewById(R.id.drawer_layout)
         navView = findViewById(R.id.nav_view)
@@ -54,12 +65,40 @@ class FirebaseActivity : AppCompatActivity(), NavigationView.OnNavigationItemSel
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
                 finish()
+                Toast.makeText(this, "Wylogowano pomyślnie", Toast.LENGTH_LONG).show()
             }
         }
         return super.onOptionsItemSelected(item)
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        when(item?.itemId){
+            R.id.nav_games ->{
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, GamesFragment())
+                    .commitNow()
+                toolbar.title = "Gry"
+            }
+            R.id.nav_movies ->{
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MoviesFragment())
+                    .commitNow()
+                toolbar.title = "Filmy"
+            }
+            R.id.nav_series ->{
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, SeriesFragment())
+                    .commitNow()
+                toolbar.title = "Seriale"
+            }
+            R.id.nav_books ->{
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, BooksFragment())
+                    .commitNow()
+                toolbar.title = "Książki"
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return super.onOptionsItemSelected(item)
     }
 }
